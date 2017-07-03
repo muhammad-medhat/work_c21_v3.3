f7f98f4aeb7ae3af607c8c9d663a6cb5772bc069
muhammad medhat <muhammad.medhat@gmail.com>
2017-07-03 21:22:41 +0200

    settings

--- application/views/app/settings/default.php
+++ application/views/app/settings/default.php
@@ -15,9 +15,10 @@ fieldset{border:solid 1px;border-radius: 5px;}
       <table class='set-content'>
 <?php   foreach($settings as $s) { ?>
 <?php      if($s->settings_group == $g->id){ ?>
-              <?=form_input(array('type'=>'hidden', 'name'=>'id[]', 'value'=>$s->id) )?>
+                  
+                  <?php if($s->enabled){ ?>
+                  <?=form_input(array('type'=>'hidden', 'name'=>'id[]', 'value'=>$s->id) )?>
                   <tr>
-                    <!--<td><?= $s->id ?></td> -->
                     <?php if($g->notes) {?>
 
                       <td class='chk'><?= form_checkbox(
@@ -25,7 +26,7 @@ fieldset{border:solid 1px;border-radius: 5px;}
                       </td>
 
                     <?php } ?>
-                    <td><?= $s->arabic  ?></td>
+                    <td><?= $s->arabic   ?></td>
                     <td>
                       <?php switch($s->html){  
                           case 'ddl':
@@ -34,6 +35,21 @@ fieldset{border:solid 1px;border-radius: 5px;}
                           case'password': 
                             echo form_password('val[]', $s->value, 'style="padding:0"');
                             break;
+                          case 'checkbox':
+                            echo form_checkbox( 
+                              array(
+                                  'name'=>'val[]', 
+                                  'value'=>$s->id, 
+                                  'checked'=>$s->value, 
+                                  'title'=>$g->notes ) );
+                            break;
+                          case'inv_approx':
+                            echo form_dropdown('val[]', array(
+                              1=>'Nothing', 
+                              2=>'half', 
+                              4=>'quarter'), 
+                            $s->value);
+                            break;
                           default:
                             echo form_input(array('type'=>'number', 'name'=>'val[]','value'=>$s->value ));
                             break;
@@ -41,7 +57,7 @@ fieldset{border:solid 1px;border-radius: 5px;}
                         } ?>
                     </td>
                   </tr>
-
+                  <?php } ?>
 <?php      } ?>
 
 <?php   } ?>
